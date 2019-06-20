53dca3fa9f3716816778b36636f98c5d0de98365
Navee <naveenkumar58957@gmail.com>
2019-06-20 23:29:59 +0530

    /New js files added commit

---
+++ tube_application/WebContent/js/main.js
@@ -0,0 +1,9 @@
+function getUserAndPassword(){
+	var user = document.getElementById('your_name').value;
+	var password = document.getElementById('your_pass').value;
+	alert(user);
+	alert(password);
+}
+function validateUser(){
+	getUserAndPassword();
+}
